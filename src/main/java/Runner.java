07d67bd6dbfac73ab92f3aae8a4540b158351222
Yohannes Itanna <itannayohannes@gmail.com>
2025-03-06 04:58:44 +0000

    Completed Lab Static Keyword

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,6 @@
+class Class {
+    public static String global_var;
+}
 
 public class Runner {
     /**
@@ -21,7 +24,7 @@ public class Runner {
      *
      * @param in the String value you should change global_var to in.
      */
-    public void changeClassVar(String in){
-
+     public void changeClassVar(String in){
+        Class.global_var = in;
     }
 }
